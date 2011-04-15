79c4dcaec89502957cee84d1a050b4b43870d314
Antoine Meunier <kameleon@productivnation.com>
2011-04-14 21:22:32 -0400

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   #return a title on a per-page basis                              # Doc comment
+
   def title                                                        # method definition
     base_title = "Ruby on Rails Tutorial Sample App"               # variable assignment
     if @title.nil?                                                 # boolean test for nil
